2e874dabe407e610a4e58a4bf7cbc39182cd470a
BandiNiharika <niharikabandi0103@gmail.com>
2025-05-10 18:11:45 +0530

    Auto commit on -- 18:11

--- auto-push.bat
+++ auto-push.bat
@@ -1,6 +1,17 @@
 @echo off
-set /p msg="Enter commit message: "
+for /f "tokens=1-4 delims=/ " %%a in ("%date%") do (
+    set month=%%a
+    set day=%%b
+    set year=%%c
+)
+for /f "tokens=1-2 delims=: " %%a in ("%time%") do (
+    set hour=%%a
+    set minute=%%b
+)
+set msg=Auto commit on %year%-%month%-%day% %hour%:%minute%
 git add .
 git commit -m "%msg%"
 git push origin main
 pause
+
+
